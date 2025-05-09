c5526ebea56aa9e9f3ea10b00787ba381625a668
Niranjan <146432443+Niranjan160@users.noreply.github.com>
2025-05-09 20:43:08 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -75,21 +75,21 @@ const Profile = ({ userId }) => {
 
         userData.rating = rating.data;
         // Fetch profile image after user data
-        try {
-          const imgRes = await axios.get(`/api/users/${userId}/profile-image`, {
-            responseType: "arraybuffer",
-          });
+        // try {
+        //   const imgRes = await axios.get(`/api/users/${userId}/profile-image`, {
+        //     responseType: "arraybuffer",
+        //   });
 
-          const base64Image = btoa(
-            new Uint8Array(imgRes.data).reduce(
-              (data, byte) => data + String.fromCharCode(byte),
-              ""
-            )
-          );
-          userData.profileImage = `data:image/webp;base64,${base64Image}`;
-        } catch (imgErr) {
-          console.warn("No profile image found or error loading it");
-        }
+        //   const base64Image = btoa(
+        //     new Uint8Array(imgRes.data).reduce(
+        //       (data, byte) => data + String.fromCharCode(byte),
+        //       ""
+        //     )
+        //   );
+        //   userData.profileImage = `data:image/webp;base64,${base64Image}`;
+        // } catch (imgErr) {
+        //   console.warn("No profile image found or error loading it");
+        // }
 
         setUser(userData);
       } catch (err) {
@@ -116,7 +116,7 @@ const Profile = ({ userId }) => {
     if (userId) {
       fetchUser();
       fetchJobs();
-      // fetchProfileImage();
+      fetchProfileImage();
       console.log("user: ",user);
     }
   }, [userId]);
