e9a10c7dee95eecbb17c9101641441544c050631
Niranjan <146432443+Niranjan160@users.noreply.github.com>
2025-05-11 18:08:17 +0530

    Update Navbar.jsx "auto reload the addjob page"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,9 +32,18 @@ const Navbar = () => {
             <NavLink to="/jobs" className={linkClass}>
               Jobs
             </NavLink>
-            <NavLink to="/add-job" className={linkClass}>
-              Post Job
-            </NavLink>
+            <NavLink
+  to="/add-job"
+  className={linkClass}
+  onClick={() => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 0);
+  }}
+>
+  Post Job
+</NavLink>
+
 
             {/* User Profile or Login */}
             {userId ? (
